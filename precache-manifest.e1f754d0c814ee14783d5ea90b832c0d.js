db2f3d9ebf2de975fffab1a0381f25805a02a525
kzchan <kzchan0816@qq.com>
2020-10-27 22:08:39 +0800

    deploy

---
+++ precache-manifest.e1f754d0c814ee14783d5ea90b832c0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ceba70c339c00b2060",
+    "url": "/kzmomey-website/css/app.46737aee.css"
+  },
+  {
+    "revision": "fd54034be953327b2e27ba7d42a03961",
+    "url": "/kzmomey-website/index.html"
+  },
+  {
+    "revision": "24ceba70c339c00b2060",
+    "url": "/kzmomey-website/js/app.3ec518ea.js"
+  },
+  {
+    "revision": "97cf1f1b9bcff9794a29",
+    "url": "/kzmomey-website/js/chunk-vendors.44657b9a.js"
+  },
+  {
+    "revision": "23dbca71938767d5d04480260290cf66",
+    "url": "/kzmomey-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/kzmomey-website/robots.txt"
+  }
+]);
